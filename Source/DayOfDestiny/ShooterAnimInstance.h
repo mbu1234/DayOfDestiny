e69db04f8bdb42d5b122301beeca4dc74ac87493
mbu1234 <89271696+mbu1234@users.noreply.github.com>
2022-10-13 19:45:50 +0100

    S3: Initial Animation
    Created a new anim instance - custom function (UpdateAnimationProperties) is called from the child  AnimBP event graph - main vars defined - speed, bIsInAir, bIsAccelerating  -  Simple statemachine added for basic running.
    Running animations need triming!

---
+++ Source/DayOfDestiny/ShooterAnimInstance.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAYOFDESTINY_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Movement, meta=(AllowPrivateAccess = "true"))
+	class AShooterCharacter* ShooterCharacter;
+
+	// Speed of the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category =Movement, meta=(AllowPrivateAccess = "True"))
+	float Speed;
+
+	// Whether or not the character is in the air
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "True"))
+	bool bIsInAir;
+
+	// Whether or not the character is moving
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "True"))
+	bool bIsAccelerating;
+
+};
